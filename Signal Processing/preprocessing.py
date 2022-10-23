825ebc483b54d8fe45febbbce4bc2111db1822ca
eleni-kapetanaki <90236651+eleni-kapetanaki@users.noreply.github.com>
2022-10-23 14:11:59 -0700

    load(), write(), fft()

--- Signal Processing/preprocessing.py
+++ Signal Processing/preprocessing.py
@@ -19,14 +19,43 @@ class File:
             Store info (first few lines of csv) in a separate variable
             Store important data in dataset variable
         """
-        # returns a tuple with as (info, dataset)
-        return (0,0)
+        if self.type == "r":
+            
+            #Extract file info (top few lines)
+            info = pd.read_csv("OpenBCI-RAW.txt", nrows=3, delimiter=" ").to_numpy()
+
+
+            #Header (Column names)
+            header = pd.read_csv("OpenBCI-RAW.txt", skiprows=4, nrows=1).to_numpy()
+
+            #Make 9 column data 2D array
+            full_data = pd.read_csv("OpenBCI-RAW.txt", skiprows=5).to_numpy()
+            time_index_data = full_data[:,:9]
+
+            #convert time index to seconds
+            if float(info[1,2])>0:
+                sample_rate = float(info[1,2])
+            else:
+                sample_rate = 250.0
+                
+            num_sample = len(time_index_data[:,0])
+            total_time = num_sample/sample_rate
+            step = 1/sample_rate
+            time = np.arange(0, total_time, step, dtype=float)
+
+            data = time_index_data.copy()
+            data[:,0] = time
+            
+            # returns a tuple with as (info, dataset)
+            return (info, data)
     
     # execute write only if type = 'w'
     def write(self, dataset: np.ndarray):
         """
             Writes filtered data file to path as csv
         """
+        if self.type == "w":
+            np.savetxt("processed_data.csv", dataset, delimiter=",")
 
 
 
@@ -50,6 +79,12 @@ class Preprocess(object):
             tuple (fft data of channels removing negative fouriers, fft frequencies)
 
         """
+        fft_data = np.fft.fft(data, axis = 0)
+        freq = np.fft.fftfreq(len(data[:,1]), d=0.004)
+        fft_data[:,0] = freq
+        
+        
+        return fft_data
         
         return 
 
