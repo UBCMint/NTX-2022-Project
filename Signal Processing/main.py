6f26ba5d0dff81832972d0fdbf88d5b155a9d40f
noor7112 <73660404+noor7112@users.noreply.github.com>
2022-10-22 17:06:42 -0700

    Updated main.py

--- Signal Processing/main.py
+++ Signal Processing/main.py
@@ -4,8 +4,7 @@
         data files in Raw Data folder obtained after collection from OpenBCI headset
         filtering frequencies
     Output:
-        filtered data file stored in Filtered Data folder
-    
+        filtered data file stored in Filtered Data folder   
 """
 import os
 import preprocessing as pp
@@ -16,20 +15,27 @@ def main():
     """
     # path to Raw Data
     rpath = os.path.join(os.path.dirname(os.getcwd()),'Data','Raw Data')
-    
+    fpath = os.path.join(os.path.dirname(os.getcwd()),'Data','Filtered Data')
     for folder in os.listdir(rpath):
         rpathexp = os.path.join(rpath,folder)
         for rfile in os.listdir(rpathexp):
             path_data_file = os.path.join(rpathexp,rfile)
             # load dataset
-            data_file = pp.File(path_data_file, 'read')
+            data_file = pp.File(path_data_file, 'r')
             (info, rdata) = data_file.load()
             # initialise dataset
-            data = pp.Preprocess(rdata)
+            p = pp.Preprocess()
+            hdata = p.hpass(rdata)
+            ldata = p.lpass(hdata)
+            ndata = p.notch(ldata)
+            p.plot(rdata, 'Raw')
+            p.plot(ndata, 'Filtered')
+            p.plot(rdata, 'FFT')
+            p.plot(ndata, 'FFT')
 
-            # hpass
-            
-
+            path_fdata_file = os.path.join(fpath,folder,rfile)
+            fdata_file =pp.File(path_fdata_file,'w')
+            fdata_file.write(ndata)
 
 if __name__ == '__main__':
     main()
