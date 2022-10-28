e6fbdb817baf80f8599397024622f2b1c68f5112
noor7112 <73660404+noor7112@users.noreply.github.com>
2022-10-27 21:52:19 -0700

    Update main.py

--- Signal Processing/main.py
+++ Signal Processing/main.py
@@ -21,20 +21,24 @@ def main():
         for rfile in os.listdir(rpathexp):
             path_data_file = os.path.join(rpathexp,rfile)
             # load dataset
-            data_file = pp.File(path_data_file, 'r')
+            data_file = pp.File(path_data_file)
             (info, rdata) = data_file.load()
             # initialise dataset
             p = pp.Preprocess()
             hdata = p.hpass(rdata)
             ldata = p.lpass(hdata)
-            ndata = p.notch(ldata)
+            # remove powerline noise using notch filter
+            # powerline noise in Canada at 60Hz
+            # source: https://www.canada.ca/en/health-canada/services/health-risks-safety/radiation/everyday-things-emit-radiation/power-lines-electrical-appliances.html 
+            ndata = p.notch(ldata, f = [60])
             p.plot(rdata, 'Raw')
             p.plot(ndata, 'Filtered')
             p.plot(rdata, 'FFT')
             p.plot(ndata, 'FFT')
 
-            path_fdata_file = os.path.join(fpath,folder,rfile)
-            fdata_file =pp.File(path_fdata_file,'w')
+            pfile = "filtered_"+rfile
+            path_fdata_file = os.path.join(fpath,folder,pfile)
+            fdata_file =pp.File(path_fdata_file)
             fdata_file.write(ndata)
 
 if __name__ == '__main__':
